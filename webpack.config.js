0c961251288d62050752ad34ca6d0974c9092c7f
Tom Ari <freak.tom3@gmail.com>
2022-03-09 09:26:26 +0200

    webpack template

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "./src/javascript/index.js"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: "asset/resource" },
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: "asset/inline" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "The Weather",
+      template: path.resolve(__dirname, "./src/template.html"),
+      filename: "index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+};
