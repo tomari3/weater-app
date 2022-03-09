4e9e5cf3113c3a0d70fa4047ea24fbb19fbc87fe
Tom Ari <freak.tom3@gmail.com>
2022-03-09 09:51:24 +0200

    basic fetch works

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,13 @@
-console.log("YEA");
+import "babel-polyfill";
+
+async function getWeather() {
+  const url =
+    "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=4f1a5f803edc74651cef27d64b2b6c51";
+  const response = await fetch(url);
+  const weatherData = await response.json();
+  console.log(weatherData);
+  console.log(weatherData.name);
+  console.log(weatherData.main.temp);
+}
+
+getWeather();
